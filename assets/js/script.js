9070cff37c38c110bc51fa09a1c89d57f0b33b4c
fehliimaa <felipelimasilva285@gmail.com>
2018-09-10 16:32:40 -0300

    modificações nos arquivos HTML, CSS e JS; deixando o site responsivo

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,3 +39,16 @@ function mudarSlide(pos) {
  */ 
 }
 
+function toggleMenu() {
+
+	var menu = document.getElementById("menu");
+
+	if (menu.style.display == 'none' || menu.style.display == ''	) {
+		menu.style.display = "block";
+	} else {
+		menu.style.display = "none";
+	}
+
+}
+
+
